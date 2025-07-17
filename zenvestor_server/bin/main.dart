fd23360f8d53f08d2d514553c0c180416e02db2f
pentaxis93 <13393192+pentaxis93@users.noreply.github.com>
2025-07-17 08:34:28 +0200

    Initial commit

---
+++ zenvestor_server/bin/main.dart
@@ -0,0 +1,7 @@
+import 'package:zenvestor_server/server.dart';
+
+/// This is the starting point for your Serverpod server. Typically, there is
+/// no need to modify this file.
+void main(List<String> args) {
+  run(args);
+}
